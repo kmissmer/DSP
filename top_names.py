0284dce16f954a03292edb54db1382f78bcfd95f
Kaleb Missmer <missmerk@moravian.edu>
2024-06-03 12:14:46 -0400

    fixed typo

--- top_names.py
+++ top_names.py
@@ -22,7 +22,7 @@ def main(filename):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 find_names.py <filename>")
+        print("Usage: python3 top_names.py <filename>")
         sys.exit(1)
     filename = sys.argv[1]
     main(filename)
